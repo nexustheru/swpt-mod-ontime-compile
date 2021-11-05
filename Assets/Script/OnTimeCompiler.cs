a3713d0b2b1e44c686381fc41a8b83a35e109a80
Apples <sehindel_19@hotmail.com>
2021-11-05 10:08:07 +0100

    beepinex

---
+++ Assets/Script/OnTimeCompiler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
+using HarmonyLib.Tools;
+
+namespace OnTimeCompilertest
+{
+    [BepInPlugin("org.bepinex.plugins.OnTimeCompiler", "OnTimeCompiler", "1.0.0.0")]
+    class OnTimeCompiler : BaseUnityPlugin
+    {
+        private Harmony _harmonyInstance;
+        private compiler Timecompiler;
+        private void debuglog(string str = "")
+        {
+            Logger.LogInfo(str);
+        }
+        private void Awake()
+        {
+            debuglog("OnTimeCompiler is loaded!");
+
+            HarmonyFileLog.Enabled = true;
+            this.transform.parent = null;
+            DontDestroyOnLoad(this);
+            _harmonyInstance = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+            Timecompiler = new compiler();
+
+        }
+        private void Start()
+        {
+            try
+            {
+                Timecompiler.Init();
+            }
+            catch(Exception ex)
+            {
+                print(ex.Message + Environment.NewLine);
+                print(ex.StackTrace + Environment.NewLine);
+            }
+        }
+    }
+}
